24a6340abcd50f8e7bb43c796626a9d9fc641c84
GangaBhavani-4030 <gangabhavani.4030@gmail.com>
2021-07-08 16:03:34 +0530

    sorting and finding winners for all three combinations

--- Day11_flipcoin_problem.sh
+++ Day11_flipcoin_problem.sh
@@ -1,4 +1,20 @@
 #day 11 programm
+print_winner()
+{
+local -n keys=$1
+local -n values=$2
+maxmimum_value=0
+maximum_value_indexs=0
+for(( i = 0; i < ${#values[@]}; i++ ))
+do
+	if(( values[i] >= maximum_value ))
+	then
+		maximum_value=${values[i]}
+		maximum_value_indexs=$i
+	fi
+done
+echo "winning combination is 4{keys[$maximum_value_indexs]} with ${maximum_value}"
+}
 echo "enter upto which number we have to flip the coin:"
 read n
 declare -A single_combo=( 
@@ -115,3 +131,39 @@ do
     triple_combo[$combination]=$percentage
     echo "percentage of $combination is ${triple_combo[$combination]}%"
 done
+
+singlet_combinations=( ${!single_combo[@]} )
+singlet_combination_percentages=( ${single_combo[@]} )
+
+doublet_combinations=( ${!double_combo[@]} )
+doublet_combination_percentages=( ${double_combo[@]} )
+
+triplet_combinations=( ${!triple_combo[@]} )
+triplet_combination_percentages=( ${triple_combo[@]} )
+total_combinations+=( ${singlet_combinations[@]} ${doublet_combinations[@]} ${triplet_combinations[@]} )
+total_combination_percentages+=( ${singlet_combination_percentages[@]} ${doublet_combination_percentages[@]} ${triplet_combination_percentages[@]}  )
+
+echo "############################ sorting #################################"
+
+echo  "sorted singlet percentages:"
+echo "$( printf "%s\n" "${singlet_combination_percentages[@]}" | sort -n ) "
+echo  "sorted doublet percentages:"
+echo "$( printf "%s\n" "${doublet_combination_percentages[@]}" | sort -n ) "
+echo  "sorted triplet percentages:"
+echo "$( printf "%s\n" "${triplet_combination_percentages[@]}" | sort -n ) "
+
+
+echo "########################### winners ####################################"
+
+
+echo "For Singlet Combination"
+print_winner singlet_combinations singlet_combination_percentages
+echo 
+echo "For Doublet Combination"
+print_winner doublet_combinations doublet_combination_percentages
+echo
+echo "For Triplet Combination"
+print_winner triplet_combinations triplet_combination_percentages
+echo
+echo "For All Combinations"
+print_winner total_combinations total_combination_percentages
